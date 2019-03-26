af6b3acc81cd02e3e1fe0fb9582afe1db011192b
Rob O'Gara <rgo594@gmail.com>
2019-03-26 15:14:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,21 @@ count = 0
     hash.each do |k,v|
       if k == "age"
         count += 1
-        age += v
+        age += vdef get_average_age_for_season(data, season)
+  # code here
+age = 0
+count = 0
+  data[season].each do |hash|
+    hash.each do |k,v|
+      if k == "age"
+        count += 1
+        age += v.to_f
+      end
+    end
+  end
+
+  answer = (age/count).round
+return answer
       end
     end
   end
